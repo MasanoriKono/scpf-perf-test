cd6cfeb4e9b62ab80e1321019dbbca97f04fff2e
kono <kono@kononoMacBook-Pro.local>
2020-09-23 14:42:58 +0900

    changed the rate

--- scpf-perf-scenario-1.py
+++ scpf-perf-scenario-1.py
@@ -116,7 +116,7 @@ class CustomLoadTestShape(LoadTestShape):
     time_limit = 3600
     spawn_rate = 200
     start_user = 1000
-    stage_increment = 1000
+    stage_increment = 500
     stage_duration = 300
     cool_down_duration = 60
 
